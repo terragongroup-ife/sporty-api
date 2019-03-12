bb8f4bc9aaf422e5d28436279f16a95b5f2339d4
Your Name <you@example.com>
2019-03-12 17:10:37 +0100

    fixed heroku issue

--- Config/settings.js
+++ Config/settings.js
@@ -3,7 +3,7 @@ mongoose.set('useCreateIndex', true);
 
 mongoose.Promise = global.Promise
 
-const connectionUrl = 'mongodb://sport:sport123@ds159025.mlab.com:59025/sport-quiz'
+const connectionUrl = "mongodb://sport:sport123@ds159025.mlab.com:59025/sport-quiz"
 mongoose.connect(connectionUrl, {useNewUrlParser: true  })
 .then(() => {
     console.log('Connected to MongoDB sucessfully')
